81b11731003e725d2b9bdb4b938840718b916876
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2025-09-28 15:25:53 +0100

    db modded

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 class Database {
     private $host = "mysql-emp-paylocity0-00.b.aivencloud.com";
+    private $port = "16646"; // use actual port from Aiven
     private $db_name = "defaultdb";
     private $username = "avnadmin";
     private $password = "AVNS_XTwQCB7wiuay88BHwOB";
@@ -9,7 +10,12 @@ class Database {
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/config/ca.pem', // download from Aiven
+            ];
+            $this->conn = new PDO($dsn, $this->username, $this->password, $options);
             $this->conn->exec("set names utf8");
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
