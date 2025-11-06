b7bbe505a8e039f9d69720ea260d3aecfa6caf12
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2025-11-06 22:41:46 +0100

    minus cloak

--- candidate-dashboard.php
+++ candidate-dashboard.php
@@ -523,6 +523,6 @@ $page_title = "Onboarding Dashboard - Career Portal";
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/cloak-loader.js"></script>
+    <!-- <script src="js/cloak-loader.js"></script> -->
 </body>
 </html>
